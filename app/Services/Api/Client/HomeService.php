a603dd70052964107c4bdccd26737c385d370183
osamarafa1994 <osama@motaweron.com>
2023-06-21 10:45:01 +0300

    user Persons type

--- app/Services/Api/Client/HomeService.php
+++ app/Services/Api/Client/HomeService.php
@@ -46,14 +46,16 @@ class HomeService
         $search_key = $request->search_key;
 //        dd($request->provider_id);
         $providers = User::when($request->provider_type,function ($query) use($request){
-                            return $query->where('provider_type',$request->provider_type);
-                             })->when($request->translation_type_id,function ($query) use($request){
-                                return $query->where('translation_type_id',$request->translation_type_id);
-                            })->when($request->city_id,function ($query) use($request){
-                                return $query->where('city_id',$request->city_id);
-                            })->when($request->person_type,function ($query) use($request){
-                                return $query->where('person_type',$request->person_type);
-                            })->get();
+            return $query->where('provider_type',$request->provider_type);
+        })->when($request->translation_type_id,function ($query) use($request){
+            return $query->where('translation_type_id',$request->translation_type_id);
+        })->when($request->city_id,function ($query) use($request){
+            return $query->where('city_id',$request->city_id);
+        })->when($request->search_key,function ($query) use($request){
+            return $query->where('name','like',"%".$request->search_key."%");
+        })->when($request->person_type,function ($query) use($request){
+            return $query->where('person_type',$request->person_type);
+        })->get();
         return helperJson(ProvidersResource::collection($providers), '',200);
     }
 
